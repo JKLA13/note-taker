ffd2b9c65748aabcb9ff3252841bcc2a1e88cf49
Jacob Adamson <jakeladamson@gmail.com>
2021-08-26 15:42:51 -0600

    updated server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
 
@@ -7,5 +8,8 @@ const PORT = process.env.PORT || 3005;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+//routing
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
