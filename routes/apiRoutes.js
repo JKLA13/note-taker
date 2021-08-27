824e9073a9132c07dcc45be60a027c7f314bcaa2
Jacob Adamson <jakeladamson@gmail.com>
2021-08-26 22:02:58 -0600

    wrote fs read and write class, still buggy

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,9 +14,10 @@ router.post("/notes", (req, res) => {
   const notes = req.body;
   notes.id = nid();
   //json for notes pushed to db
-  db.push(notes);
+  noteSave.push(notes);
   console.log(notes, "Your note was added!");
   res.json(true);
+  console.log(noteSave);
 });
 //delete notes
 router.delete("/notes/:id", (req, res) => {
@@ -25,17 +26,35 @@ router.delete("/notes/:id", (req, res) => {
   let i = db[i].id === deleteData ? (db.splice[i], 1) : res.end();
 });
 
-// class noteDb {
-//   readNotes() {
-//     fs.readFileSync("../db/db.json", "utf8");
-//   }
-//   writeNotes(notes) {
-//     fs.writeFileSync("../db/db.json", JSON.stringify(notes));
-//   }
-//   getNotes() {
-//     return this.readNotes().then((notes) => {});
-//   }
-// }
+//create class for databas notes/fs read & write code
+class noteDb {
+  readNotes() {
+    fs.readFileSync("../db/db.json", "utf8");
+  }
+  writeNotes(notes) {
+    fs.writeFileSync("../db/db.json", JSON.stringify(notes));
+  }
+  getNotes() {
+    return this.readNotes().then((notes) => {});
+  }
+
+  appendNote(notes) {
+    const { title, text } = notes;
+
+    const addId = { title, text, id: nid() };
+
+    return this.getNotes()
+      .then((notes) => [...notes, addId])
+      .then((newNotes) => this.writeNotes(newNotes))
+      .then(() => addId);
+  }
+
+  deleteNote(id) {
+    return this.getNotes()
+      .then((notes) => notes.filter((notes) => notes.id !== id))
+      .then((filterNote) => this.writeNotes(filterNote));
+  }
+}
 
 //export module
 module.exports = router;
